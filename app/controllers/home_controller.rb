e84e2804aaa658371752f3d9794350e478ee2e7e
Jung <rtkm9760304@gmail.com>
2012-05-24 00:38:55 +0800

    commented befor_filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,33 +1,38 @@
 class HomeController < ApplicationController
 	include RestGraph::RailsUtil
-	before_filter :login_facebook, :only => [:login]
-	before_filter :load_facebook, :except => [:login]
+	#before_filter :login_facebook, :only => [:login]
+	#before_filter :load_facebook, :except => [:login]
 	
 	def index
       @access_token = rest_graph.access_token
       if @access_token
         @me = rest_graph.get('/me')
+        @friendlist = rest_graph.get('/friendlists')
       end
-    end
+  end
 
-    def login
-      redirect_to home_path
-    end
+  def login
+    redirect_to home_path
+  end
 
-    def logout
-      reset_session
-      redirect_to home_path
-    end
+  def logout
+    reset_session
+    redirect_to home_path
+  end
+
+  def friendlist
+
+  end
 
 private
-    def load_facebook
-      rest_graph_setup(:write_session => true)
-    end
+  def load_facebook
+    rest_graph_setup(:write_session => true)
+  end
 
-    def login_facebook
-      rest_graph_setup(:auto_authorize         => true,
-                       :auto_authorize_scope   => 'read_friendlists',
-                       :ensure_authorized      => true,
-                       :write_session          => true)
-    end  	
+  def login_facebook
+    rest_graph_setup(:auto_authorize         => true,
+                     :auto_authorize_scope   => 'read_friendlists',
+                     :ensure_authorized      => true,
+                     :write_session          => true)
+  end  	
 end
