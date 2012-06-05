d9e7a0ae25cbd55b2f85f2f43addc3099b9d16f1
Jung <rtkm9760304@gmail.com>
2012-06-05 15:17:59 +0800

    cloth-colors model

--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -2,4 +2,5 @@ class Cloth < ActiveRecord::Base
   attr_accessible :public_class, :private_class, :color, :description, :image, :privacy, :redRemark, :redTime, :signal
 
   belongs_to :user
+  has_many :colors
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120530172546) do
+ActiveRecord::Schema.define(:version => 20120605064258) do
 
   create_table "cloths", :force => true do |t|
     t.integer  "user_id"
@@ -28,8 +28,16 @@ ActiveRecord::Schema.define(:version => 20120530172546) do
     t.string   "signal"
   end
 
+  create_table "colors", :force => true do |t|
+    t.string   "color"
+    t.integer  "cloth_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "comments", :force => true do |t|
     t.text     "content"
+    t.integer  "post_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
